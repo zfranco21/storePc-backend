efde619949b1ba81a51f9d4c772366db00c8fb91
Franco Herrera <francoherrera1759@gmail.com>
2024-12-18 13:48:27 -0300

    <fixes> fixes en rutas filtros y busqueda

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -9,10 +9,12 @@ router.put("/:id", productController.updateProduct); // Actualizar un producto
 router.delete("/:id", productController.deleteProduct); // Eliminar un producto
 
 // Filtros y búsquedas
-router.get("/filter", productController.filterByStock); // Filtrar por disponibilidad de stock
-router.get("/:id", productController.getProductById); // Obtener un producto por ID
 router.get("/:categoryId/products", productController.getProductsByCategory); // Filtrar por categoría
+router.get("/filter", productController.filterByStock); // Filtrar por disponibilidad de stock
 router.get("/search", productController.searchProducts); // Buscar productos por palabras clave
+router.get("/random", productController.getRandomProducts); // Obtener productos aleatorios
+router.get("/:id", productController.getProductById); // Obtener un producto por ID
+
 // Actualizar stock
 router.put("/:productId/stock", productController.updateStock);
 // Habilitar/deshabilitar producto
